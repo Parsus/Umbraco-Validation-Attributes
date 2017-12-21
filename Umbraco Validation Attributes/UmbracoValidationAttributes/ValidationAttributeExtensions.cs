c8d3dc41951463ceb81192cfcbb7ff9cfacc5038
Rahul B. Patel <rahul.patel@parsus.com>
2017-12-21 01:10:38 -0700

    fix(ValidationAttributeExtensions): fix TrySetErrorMessage dictionary fetch
    re: #266

--- Umbraco Validation Attributes/UmbracoValidationAttributes/ValidationAttributeExtensions.cs
+++ Umbraco Validation Attributes/UmbracoValidationAttributes/ValidationAttributeExtensions.cs
@@ -3,10 +3,18 @@
 namespace UmbracoValidationAttributes {
     public static class ValidationAttributeExtensions {
         static public void TrySetErrorMessage(this ValidationAttribute attr, string dictionaryKey) {
+            if(string.IsNullOrEmpty(dictionaryKey)) { return; }
+
             var item = UmbracoValidationHelper.GetDictionaryItem(dictionaryKey);
+
             if (item != dictionaryKey) {
+                // dictionaryKey has entry in dictionary, use entry value as error message
                 attr.ErrorMessage = item;
+            } else {
+                // dictionaryKey does not have entry in dictionary, assume it is error message
+                attr.ErrorMessage = dictionaryKey;
             }
         }
     }
 }
+ 
