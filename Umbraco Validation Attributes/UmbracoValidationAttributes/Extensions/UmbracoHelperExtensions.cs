1d56c49e253c29ac5d52756467aadebaa8c24a76
Rahul B. Patel <rahul.patel@parsus.com>
2017-12-27 12:12:10 -0700

    fix(UmbracoHelperExtensions): fix caching
    re: #331

--- Umbraco Validation Attributes/UmbracoValidationAttributes/Extensions/UmbracoHelperExtensions.cs
+++ Umbraco Validation Attributes/UmbracoValidationAttributes/Extensions/UmbracoHelperExtensions.cs
@@ -1,26 +1,40 @@
-﻿using Umbraco.Core;
+﻿using System.Collections.Generic;
+using Umbraco.Core;
 using Umbraco.Web;
 
 namespace UmbracoValidationAttributes.Extensions {
 
     public static class UmbracoHelperExtensions {
 
-        public static string GetCachedDictionaryValueOrFallback(this UmbracoHelper umbraco, string key,
-           string fallback,
+        /// <summary>
+        /// This method is terribly overloaded:
+        /// </summary>
+        /// <param name="umbraco"></param>
+        /// <param name="keyOrFallback"></param>
+        /// <param name="fallbackOrFirstFormatArg"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static string GetCachedDictionaryValueOrFallback(this UmbracoHelper umbraco, 
+           
+           string key,
+           string altText,
            params object[] args) {
 
-            var value = ApplicationContext.Current.ApplicationCache.RuntimeCache
-                .GetCacheItem("PinkDictionary_" + key,
-                () => {
-                    var result = umbraco.GetDictionaryValue(key, fallback ?? key);
-                    if (args != null && args.Length > 0) {
-                        result = string.Format(result, args);
-                    }
+            var cacheKey = string.Concat("PinkDictionary_",
+                key, 
+                "_",
+                altText);
 
-                    return result;
-                });
+            var result = ApplicationContext.Current.ApplicationCache.RuntimeCache
+                .GetCacheItem(cacheKey,
+                () => umbraco.GetDictionaryValue(key, altText))
+                .ToString();
 
-            return value.ToString();
+            if(args != null && args.Length > 0) { 
+                result = string.Format(result, args);
+            }
+
+            return result;
         }
     }
 }
